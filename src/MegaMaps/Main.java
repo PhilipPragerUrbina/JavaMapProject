73989c51d437cf5424a5db6cfbd2586d5ae31d5b
PhilipPragerUrbina <72355251+PhilipPragerUrbina@users.noreply.github.com>
2022-12-07 18:53:12 -0800

    Working hash map!

--- src/MegaMaps/Main.java
+++ src/MegaMaps/Main.java
@@ -1,6 +1,7 @@
 package MegaMaps;
 
 import MegaMaps.Benchmarks.SimpleBenchMark;
+import MegaMaps.Maps.HashMap;
 import MegaMaps.Maps.LoopMap;
 
 
@@ -8,10 +9,15 @@ import MegaMaps.Maps.LoopMap;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Map<String,Integer> map = new LoopMap<>();
+        Map<String,Integer> map = new HashMap<>(1000);
         SimpleBenchMark bench = new SimpleBenchMark(map);
 
-        System.out.println("Time(ms): "+ bench.run(10000));
+        System.out.println("Time(ms) hashmap: "+ bench.run(50000)); //57 ms
+
+        Map<String,Integer> map2 = new LoopMap<>();
+        SimpleBenchMark bench2 = new SimpleBenchMark(map2);
+
+        System.out.println("Time(ms) simple: "+ bench2.run(50000)); //12920 ms
 
     }
 }
--- src/MegaMaps/Maps/HashMap.java
+++ src/MegaMaps/Maps/HashMap.java
@@ -2,32 +2,114 @@ package MegaMaps.Maps;
 
 import MegaMaps.Map;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
- * A Hash Map
+ * A hash map implementation
  * @param <KeyType>
  * @param <ValueType>
  */
 public class HashMap<KeyType, ValueType> implements Map<KeyType,ValueType> {
-    private ValueType[] values;
-    int size= 0;
 
+    /**
+     * The Bucket class for putting together similar keys
+     */
+    private class Bucket{
+        public ArrayList<Pair> pairs = new ArrayList<>(); //the contents fo the bucket
+        public Bucket(Pair first_pair){ //Create the bucket with a pair
+            addPair(first_pair); //must have one pair
+        }
+        public void addPair(Pair pair){ //add a pair to the bucket
+          pairs.add(pair); //todo nested buckets
+        }
+    }
+
+    /**
+     * Represents a key value pair
+     */
+    private class Pair{
+        public Pair( KeyType key,ValueType value) { //simple constructor
+            this.value = value;
+            this.key = key;
+        }
+        public ValueType value;
+        public KeyType key;
+    }
+
+    //todo add auto resize and re-hash. Multi-thread?
+    private Bucket[] buffer; //actual hash map array
+    private int entry_size = 0; //the # of entries
+
+    /**
+     * Create a new hash map with a certain size
+     * @param start_size Should be similar to the amount of data you expect to contain in it
+     */
     public HashMap(int start_size){
-        values = (ValueType[]) new Object[start_size]; //todo why
+        buffer = (Bucket[]) Array.newInstance(Bucket.class, start_size); //todo Find out a better way to do this
     }
 
     @Override
     public ValueType get(KeyType key) {
 
-        int hash = key.hashCode();
-        long idx = hash % n;
-        return  null; //none found
+        int hash = key.hashCode(); //create the hash code
+        int idx =getIdx(hash); //get the index
+
+        Bucket bucket = buffer[idx]; //access the bucket
+        if(bucket != null){ //exists
+                for (Pair candidate: bucket.pairs) {
+                    if(candidate.key.equals(key)){
+                        return candidate.value; //find corresponding value
+                    }
+                }
+        }
+
+        return null; //none found
     }
 
     @Override
     public void put(KeyType key, ValueType value) {
 
+        int hash = key.hashCode(); //get the hash code
+        int idx =getIdx(hash); //get the index
+
+        Pair pair = new Pair(key, value); //Create the pair to insert
+        entry_size++; //update size
+
+        if(buffer[idx] != null){    //there is an entry here
+            for (Pair other : buffer[idx].pairs) {
+                if(other.key.equals(key)){  //assignment
+                    other.value = value;
+                    return;
+                }
+            }
+            //else collision
+            buffer[idx].addPair(pair);
+
+        }else{ //first entry
+            buffer[idx] = new Bucket(pair);
+        }
+
+    }
+
+    /**
+     * Get the size of the map
+     * @return # of entries
+     */
+    public int size(){
+        return entry_size;
+    }
+
+    //todo the key set
+
+    /**
+     * Utility to get the index in the buffer array
+     * @param hash The hashed key
+     * @return The index
+     */
+    private int getIdx(int hash){
+        //should not be negative
+        return Math.abs(hash % buffer.length); //todo make sure to minimize collisions
     }
 
 
