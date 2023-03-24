b2ee0629419cb5141ab50636ec23240e5dc7cb12
ppragerurbina788 <ppragerurbina788@student.fuhsd.org>
2023-03-24 15:03:40 -0700

    Added towers puzzle

--- src/MegaMaps/Sorting/RecursionMain.java
+++ src/MegaMaps/Sorting/RecursionMain.java
@@ -1,7 +1,9 @@
 package MegaMaps.Sorting;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+//Recursive puzzles
 public class RecursionMain {
 
     public static void main(String[] args) {
@@ -12,6 +14,61 @@ public class RecursionMain {
                 ,0
         ));
         System.out.println(queen8(new int[8], 0));
+
+       hanoiTower(4);
+    }
+
+    /**
+     * Print towers of hanoi instructions
+     * @param count Number of discs
+     */
+    public static void hanoiTower(int count){
+        //array is zero initialized, which is where all the discs start
+        hanoiTower(count-1,new int[count], 0,2);
+    }
+
+    /**
+     * Towers of hanoi
+     * Move pegs from peg A to peg C,Disk can not be on smaller disk,Only disk at top of peg can be moved
+     * @param n (Number of disks)-1
+     * @param discs Disc positions(pin index) in order of increasing disk size. The disc array is not necessary and is only for visualization purposes.
+     * @param start Start pin index 0,1,2
+     * @param target End pin index 0,1,2
+     */
+    public static void hanoiTower(int n, int[] discs,int start,int target){
+        //discs have tower id and are in increasing order
+        if(n == -1) return; //No more discs
+        int helper_pin =  3 - (start+target); //Get pin that is neither target, nor start
+        hanoiTower(n-1,discs,start, helper_pin); //Move to helper pin
+        System.out.println("Move disk " + n+1 + " to  pin " + target+1); //Output instruction(With normal person counts rather than indices)
+        printDisks(discs);
+        discs[n] = target;//move
+        hanoiTower(n-1, discs,helper_pin,target); //Move to target
+    }
+   static void printDisks(int[] disks){
+        for (int n = 0; n < disks.length; n++) {
+            for (int j = 0; j < 3; j++) {
+                if(disks[n] == j){
+                    int width = (n+1)*2;
+                    int total_width = (disks.length)*2;
+                    for (int i = 0; i < total_width/2 - width; i++) {
+                        System.out.print(" ");
+                    }
+                    for (int i = total_width/2 - width; i < total_width/2 + width; i++) {
+                        System.out.print("-");
+                    }
+                    for (int i = total_width/2 + width; i < total_width+6; i++) {
+                        System.out.print(" ");
+                    }
+                }else{
+                    for (int i = 0; i <= disks.length*2+5; i++) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+
+        }
     }
 
 
